6ccd5f60af6e3e4c5e986573c95bc3ddd62a2d87
Parth Patel <parth@pardypanda.com>
2017-02-27 12:20:56 +0530

    init

---
+++ FantasticView/Fanatastic.swift
@@ -0,0 +1,40 @@
+//
+//  Fanatastic.swift
+//  FantasticView
+//
+//  Created by Tanay Shah on 09/02/17.
+//  Copyright © 2017 Tanay Shah. All rights reserved.
+//
+
+import UIKit
+
+class Fantastic: UIView
+{
+    let colors : [UIColor] = [.red, .orange, .yellow, .green, .blue, .purple]
+    var colorCounter = 0
+    
+    override init(frame: CGRect)
+    {
+        super.init(frame: frame)
+        
+        if #available(iOS 10.0, *) {
+            let scheduledColorChanged = Timer.scheduledTimer(withTimeInterval: 2.0, repeats: true) { (timer) in
+                UIView.animate(withDuration: 1.0) {
+                    self.layer.backgroundColor = self.colors[self.colorCounter % 6].cgColor
+                    self.colorCounter+=1
+                }
+            }
+            scheduledColorChanged.fire()
+        } else {
+            // Fallback on earlier versions
+        }
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        
+        // You don't really need to implement this
+    }
+}
--- FantasticView/ViewController.swift
+++ FantasticView/ViewController.swift
@@ -8,16 +8,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let fantasticView = Fantastic(frame: self.view.bounds)
+        
+        self.view.addSubview(fantasticView)
     }
-
-    override func didReceiveMemoryWarning() {
+    
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
