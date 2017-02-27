f49c5fbff0fe2a46652b41267e969e2f08214250
Parth Patel <parth@pardypanda.com>
2017-02-27 12:11:05 +0530

    Initial Commit

---
+++ FantasticView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FantasticView
+//
+//  Created by Tanay Shah on 27/02/17.
+//  Copyright © 2017 Tanay Shah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
